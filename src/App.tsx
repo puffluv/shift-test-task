3e5c7f7370b7ae727dae7a76a29cd3c7f52a80b7
puffluv <alibihasenov@gmail.com>
2024-10-15 00:08:19 +0700

    Init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import AuthForm from "./components/AuthForm/AuthForm";
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <AuthForm />
+      </div>
+    </>
+  );
+}
+
+export default App;
