4a61d530ef8436ac935c6507727003c548484052
puffluv <alibihasenov@gmail.com>
2024-10-15 00:14:06 +0700

    Update styles

--- src/components/AuthForm/AuthForm.tsx
+++ src/components/AuthForm/AuthForm.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useForm } from "react-hook-form";
 import axios from "axios";
-import "./index.css";
+import "./AuthForm.modules.css";
 
 interface FormData {
   phone: string;
