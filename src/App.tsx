ff313008e43a42f508840feee644b65fe90053fb
puffluv <alibihasenov@gmail.com>
2024-10-15 19:31:41 +0700

    Add provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
+import { Provider } from "react-redux";
 import "./App.css";
-import AuthForm from "./components/AuthForm/AuthForm";
+import AuthForm from "./components/AuthForm";
+import store from "./store";
 
 function App() {
   return (
-    <>
+    <Provider store={store}>
       <div className="App">
         <AuthForm />
       </div>
-    </>
+    </Provider>
   );
 }
 
